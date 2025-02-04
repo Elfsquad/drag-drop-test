3d154b52177e8450fe204da14217b3ed9fcee454
Stan van Rooy <me@stanvanrooy.com>
2025-02-04 17:45:21 +0100

    v2, gh pages

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-  console.log('button clicked');
-});
-
-
 let draggingData = null;
 let draggingPosition = { x: 0, y: 0 };
 
